14be8d8d11b270d797432cddea84203b76e123de
Rupert Smith <rupertlssmith@github.com>
2016-12-05 15:37:32 +0000

    Preparing an example to run under Nashorn.

---
+++ Main.elm
@@ -0,0 +1,46 @@
+module Main exposing (example)
+
+import HtmlToString exposing (htmlToString)
+import Html exposing (Html)
+import Json.Encode as Encode exposing (Value)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = None
+
+
+type alias StaticProgram =
+    Platform.Program Value Model Msg
+
+
+init : Value -> ( Model, Cmd Msg )
+init model =
+    ( {}, Cmd.none )
+
+
+main : StaticProgram
+main =
+    Html.programWithFlags
+        { view = view
+        , init = init
+        , subscriptions = \_ -> Sub.none
+        , update = update
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update action model =
+    ( model, Cmd.none )
+
+
+example =
+    htmlToString <| view {}
+
+
+view : Model -> Html Msg
+view model =
+    Html.div [] [ Html.text "hello world" ]
