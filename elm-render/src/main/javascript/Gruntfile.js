b7d7a39e9b7898d6fe782d536af62daa15f522ec
Rupert Smith <rupertlssmith@github.com>
2016-12-07 11:24:39 +0000

    Getting the grunt build to work.

--- elm-render/src/main/javascript/Gruntfile.js
+++ elm-render/src/main/javascript/Gruntfile.js
@@ -2,33 +2,15 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-html2js');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-task');
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-ng-annotate');
-    grunt.loadNpmTasks('grunt-angular-templates');
     grunt.loadNpmTasks('grunt-elm');
     grunt.loadNpmTasks('grunt-exec');
 
     grunt.initConfig({
         'pkg': grunt.file.readJSON('package.json'),
-
-        'bower': {
-            install: {
-                options: {
-                    install: true,
-                    copy: false,
-                    targetDir: 'assets/bower_components',
-                    cleanTargetDir: false
-                }
-            }
-        },
 
         'exec': {
             'elm-github-install': {
@@ -49,32 +31,7 @@ module.exports = function(grunt) {
 
         'copy': {
             'dist': {
-                files: [{
-                    expand: true,
-                    cwd: 'src/styles',
-                    src: ['**'],
-                    dest: 'app/styles'
-                }, {
-                    expand: true,
-                    cwd: 'bower_components/thesett-laf/app',
-                    src: ['**'],
-                    dest: 'app'
-                }, {
-                    expand: true,
-                    cwd: 'src',
-                    src: ['index.html'],
-                    dest: 'app'
-                }, {
-                    expand: true,
-                    cwd: 'bower_components/',
-                    src: ['**'],
-                    dest: 'app/libs'
-                }, {
-                    expand: true,
-                    cwd: 'src/webcomponents',
-                    src: ['**'],
-                    dest: 'app'
-                }],
+                files: [],
             }
         },
 
@@ -101,43 +58,6 @@ module.exports = function(grunt) {
             }
         },
 
-        'responsive_images': {
-            'dist': {
-                options: {
-                    engine: 'im',
-                    quality: '25',
-                    sizes: [{
-                        width: '100%',
-                        name: 'large',
-                        suffix: '.x2'
-                    }, {
-                        width: '66%',
-                        name: 'medium',
-                        suffix: '.x2'
-                    }, {
-                        width: '44%',
-                        name: 'small',
-                        suffix: '.x2'
-                    }, {
-                        width: '50%',
-                        name: 'large'
-                    }, {
-                        width: '33%',
-                        name: 'medium'
-                    }, {
-                        width: '22%',
-                        name: 'small'
-                    }]
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'src/images',
-                    src: ['**/*.{jpg,gif,png}'],
-                    dest: 'app/images'
-                }]
-            }
-        },
-
         'compress': {
             dist: {
                 options: {
@@ -150,19 +70,9 @@ module.exports = function(grunt) {
             }
         },
 
-        'connect': {
-            server: {
-                options: {
-                    hostname: 'localhost',
-                    port: 9170,
-                    base: 'app'
-                }
-            }
-        },
-
         'watch': {
             'dev': {
-                files: ['Gruntfile.js', 'bower.json', 'elm-package.json', 'server.js', 'config.rb', 'src/**'],
+                files: ['Gruntfile.js', 'elm-package.json', 'src/**'],
                 tasks: ['build'],
                 options: {
                     atBegin: true
@@ -179,13 +89,13 @@ module.exports = function(grunt) {
 
         'clean': {
             temp: {
-                src: ['tmp', 'app', 'dist', "bower_components", "elm-stuff"]
+                src: ['tmp', 'app', 'dist', "elm-stuff"]
             }
         },
     });
 
-    grunt.registerTask('dev', ['bower', 'connect:server', 'watch:dev']);
-    grunt.registerTask('minified', ['bower', 'connect:server', 'watch:min']);
-    grunt.registerTask('build', ['bower', 'copy', 'exec:elm-github-install', 'elm', 'responsive_images']);
+    grunt.registerTask('dev', ['watch:dev']);
+    grunt.registerTask('minified', ['watch:min']);
+    grunt.registerTask('build', ['copy', 'exec:elm-github-install', 'elm']);
     grunt.registerTask('package', ['build', 'exec:closure', 'uglify', 'compress']);
 };
