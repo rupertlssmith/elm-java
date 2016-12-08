bc5821ef963edf41aa91ffb428da1d1585585c18
Rupert Smith <rupertlssmith@github.com>
2016-12-08 16:40:34 +0000

    Using the mojo for installing node and npm as the base for installing elm.

--- elm-java-maven-plugin/src/main/java/com/thesett/elm/maven/InstallElmMojo.java
+++ elm-java-maven-plugin/src/main/java/com/thesett/elm/maven/InstallElmMojo.java
@@ -15,11 +15,20 @@
  */
 package com.thesett.elm.maven;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoFailureException;
+import com.github.eirslett.maven.plugins.frontend.lib.FrontendPluginFactory;
+import com.github.eirslett.maven.plugins.frontend.lib.InstallationException;
+import com.github.eirslett.maven.plugins.frontend.lib.NPMInstaller;
+import com.github.eirslett.maven.plugins.frontend.lib.NodeInstaller;
+import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
+import com.github.eirslett.maven.plugins.frontend.mojo.AbstractFrontendMojo;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.crypto.SettingsDecrypter;
 
 /**
  * InstallElmMojo builds on the frontend-maven-plugin to add support for installing elm build tools.
@@ -33,29 +42,121 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 
 @Mojo(name = "install-elm", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true)
-public class InstallElmMojo extends AbstractMojo
+public class InstallElmMojo extends AbstractFrontendMojo
 {
     /** The version of Elm to install. */
     @Parameter(property = "elmVersion", required = true)
     private String elmVersion;
 
+    /** Where to download Node.js binary from. Defaults to http://nodejs.org/dist/ */
+    @Parameter(
+        property = "nodeDownloadRoot", required = false, defaultValue = NodeInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT
+    )
+    private String nodeDownloadRoot;
+
+    /** Where to download NPM binary from. Defaults to http://registry.npmjs.org/npm/-/ */
+    @Parameter(property = "npmDownloadRoot", required = false, defaultValue = NPMInstaller.DEFAULT_NPM_DOWNLOAD_ROOT)
+    private String npmDownloadRoot;
+
+    /**
+     * Where to download Node.js and NPM binaries from.
+     *
+     * @deprecated use {@link #nodeDownloadRoot} and {@link #npmDownloadRoot} instead, this configuration will be used
+     *             only when no {@link #nodeDownloadRoot} or {@link #npmDownloadRoot} is specified.
+     */
+    @Parameter(property = "downloadRoot", required = false, defaultValue = "")
+    @Deprecated
+    private String downloadRoot;
+
+    /**
+     * The version of Node.js to install. IMPORTANT! Most Node.js version names start with 'v', for example 'v0.10.18'
+     */
+    @Parameter(property = "nodeVersion", required = true)
+    private String nodeVersion;
+
+    /** The version of NPM to install. */
+    @Parameter(property = "npmVersion", required = false, defaultValue = "provided")
+    private String npmVersion;
+
+    /** Server Id for download username and password */
+    @Parameter(property = "serverId", defaultValue = "")
+    private String serverId;
+
+    @Parameter(property = "session", defaultValue = "${session}", readonly = true)
+    private MavenSession session;
+
     /** Skips execution of this mojo. */
     @Parameter(property = "skip.installnodenpm", defaultValue = "false")
     private Boolean skip;
 
-    /** {@inheritDoc} */
-    public void execute() throws MojoFailureException
+    @Component(role = SettingsDecrypter.class)
+    private SettingsDecrypter decrypter;
+
+    @Override
+    public void execute(FrontendPluginFactory factory) throws InstallationException
     {
-        System.out.println("execute");
-        
-        if (!skipExecution())
+        ProxyConfig proxyConfig = MojoUtils.getProxyConfig(session, decrypter);
+        String nodeDownloadRoot = getNodeDownloadRoot();
+        String npmDownloadRoot = getNpmDownloadRoot();
+        Server server = MojoUtils.decryptServer(serverId, session, decrypter);
+
+        if (null != server)
         {
+            factory.getNodeInstaller(proxyConfig)
+                .setNodeVersion(nodeVersion)
+                .setNodeDownloadRoot(nodeDownloadRoot)
+                .setNpmVersion(npmVersion)
+                .setUserName(server.getUsername())
+                .setPassword(server.getPassword())
+                .install();
+            factory.getNPMInstaller(proxyConfig)
+                .setNodeVersion(nodeVersion)
+                .setNpmVersion(npmVersion)
+                .setNpmDownloadRoot(npmDownloadRoot)
+                .setUserName(server.getUsername())
+                .setPassword(server.getPassword())
+                .install();
+        }
+        else
+        {
+            factory.getNodeInstaller(proxyConfig)
+                .setNodeVersion(nodeVersion)
+                .setNodeDownloadRoot(nodeDownloadRoot)
+                .setNpmVersion(npmVersion)
+                .install();
+            factory.getNPMInstaller(proxyConfig)
+                .setNodeVersion(this.nodeVersion)
+                .setNpmVersion(this.npmVersion)
+                .setNpmDownloadRoot(npmDownloadRoot)
+                .install();
         }
     }
 
-    /** {@inheritDoc} */
+    @Override
     protected boolean skipExecution()
     {
         return this.skip;
+    }
+
+    private String getNodeDownloadRoot()
+    {
+        if ((downloadRoot != null) && !"".equals(downloadRoot) &&
+                NodeInstaller.DEFAULT_NODEJS_DOWNLOAD_ROOT.equals(nodeDownloadRoot))
+        {
+            return downloadRoot;
+        }
+
+        return nodeDownloadRoot;
+    }
+
+    private String getNpmDownloadRoot()
+    {
+        if ((downloadRoot != null) && !"".equals(downloadRoot) &&
+                NPMInstaller.DEFAULT_NPM_DOWNLOAD_ROOT.equals(npmDownloadRoot))
+        {
+            return downloadRoot;
+        }
+
+        return npmDownloadRoot;
     }
 }
---
+++ elm-java-maven-plugin/src/main/java/com/thesett/elm/maven/MojoUtils.java
@@ -0,0 +1,126 @@
+package com.thesett.elm.maven;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.github.eirslett.maven.plugins.frontend.lib.ProxyConfig;
+
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.settings.Proxy;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest;
+import org.apache.maven.settings.crypto.SettingsDecrypter;
+import org.apache.maven.settings.crypto.SettingsDecryptionResult;
+import org.codehaus.plexus.util.Scanner;
+import org.codehaus.plexus.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+class MojoUtils
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(MojoUtils.class);
+
+    static <E extends Throwable> MojoFailureException toMojoFailureException(E e)
+    {
+        return new MojoFailureException(e.getMessage() + ": " + e.getCause().getMessage(), e);
+    }
+
+    static ProxyConfig getProxyConfig(MavenSession mavenSession, SettingsDecrypter decrypter)
+    {
+        if ((mavenSession == null) || (mavenSession.getSettings() == null) ||
+                (mavenSession.getSettings().getProxies() == null) || mavenSession.getSettings().getProxies().isEmpty())
+        {
+            return new ProxyConfig(Collections.<ProxyConfig.Proxy>emptyList());
+        }
+        else
+        {
+            final List<Proxy> mavenProxies = mavenSession.getSettings().getProxies();
+
+            final List<ProxyConfig.Proxy> proxies = new ArrayList<ProxyConfig.Proxy>(mavenProxies.size());
+
+            for (Proxy mavenProxy : mavenProxies)
+            {
+                if (mavenProxy.isActive())
+                {
+                    mavenProxy = decryptProxy(mavenProxy, decrypter);
+                    proxies.add(new ProxyConfig.Proxy(mavenProxy.getId(), mavenProxy.getProtocol(),
+                            mavenProxy.getHost(), mavenProxy.getPort(), mavenProxy.getUsername(),
+                            mavenProxy.getPassword(), mavenProxy.getNonProxyHosts()));
+                }
+            }
+
+            LOGGER.info("Found proxies: {}", proxies);
+
+            return new ProxyConfig(proxies);
+        }
+    }
+
+    static Server decryptServer(String serverId, MavenSession mavenSession, SettingsDecrypter decrypter)
+    {
+        if (StringUtils.isEmpty(serverId))
+        {
+            return null;
+        }
+
+        Server server = mavenSession.getSettings().getServer(serverId);
+
+        if (server != null)
+        {
+            final DefaultSettingsDecryptionRequest decryptionRequest = new DefaultSettingsDecryptionRequest(server);
+            SettingsDecryptionResult decryptedResult = decrypter.decrypt(decryptionRequest);
+
+            return decryptedResult.getServer();
+        }
+        else
+        {
+            LOGGER.warn("Could not find server '" + serverId + "' in settings.xml");
+
+            return null;
+        }
+    }
+
+    static boolean shouldExecute(BuildContext buildContext, List<File> triggerfiles, File srcdir)
+    {
+        // If there is no buildContext, or this is not an incremental build, always execute.
+        if ((buildContext == null) || !buildContext.isIncremental())
+        {
+            return true;
+        }
+
+        if (triggerfiles != null)
+        {
+            for (File triggerfile : triggerfiles)
+            {
+                if (buildContext.hasDelta(triggerfile))
+                {
+                    return true;
+                }
+            }
+        }
+
+        if (srcdir == null)
+        {
+            return true;
+        }
+
+        // Check for changes in the srcdir
+        Scanner scanner = buildContext.newScanner(srcdir);
+        scanner.scan();
+
+        String[] includedFiles = scanner.getIncludedFiles();
+
+        return ((includedFiles != null) && (includedFiles.length > 0));
+    }
+
+    private static Proxy decryptProxy(Proxy proxy, SettingsDecrypter decrypter)
+    {
+        final DefaultSettingsDecryptionRequest decryptionRequest = new DefaultSettingsDecryptionRequest(proxy);
+        SettingsDecryptionResult decryptedResult = decrypter.decrypt(decryptionRequest);
+
+        return decryptedResult.getProxy();
+    }
+}
