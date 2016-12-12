16ae87aeaa14fd5ece67cce1dd71d8e526e00592
Rupert Smith <rupertlssmith@github.com>
2016-12-12 14:41:42 +0000

    Created an outline for the dropwizard bundle for rendering views with Elm.

--- elm-render/src/main/java/com/thesett/elm/Main.java
+++ elm-render/src/main/java/com/thesett/elm/Main.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.thesett.elm;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
