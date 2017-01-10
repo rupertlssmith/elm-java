43912b40404d19dbe3e45a30a6b1f45f1a4defb4
Rupert Smith <rupertlssmith@github.com>
2017-01-10 11:26:38 +0000

    Mocked out the document sufficiently so that Navigation.program does not fail to initialize.

--- elm-render/src/main/resources/bootnashorn.js
+++ elm-render/src/main/resources/bootnashorn.js
@@ -4,6 +4,25 @@ var process = {
     env: {}
 };
 
+// This needs mocked out if Navigation.program is in the codebase, as it runs getLocation().
+// Navigation.program cannot be used server side but it will cause a failure if this is not
+// mocked out to provide it with some dummy values.
+var document = {
+    location: {
+	    href: "",
+	    host: "",
+	    hostname: "",
+	    protocol: "",
+	    origin: "",
+	    port_: "",
+	    pathname: "",
+	    search: "",
+	    hash: "",
+	    username: "",
+	    password: ""
+    }
+};
+
 var console = {};
 console.debug = print;
 console.warn = print;
